92a3950857d570b60081d59106ce44e802bc8f12
Masha <marija.samojlenko@gmail.com>
2020-09-17 15:12:08 +0300

    add eol

--- src/main/java/Lf.java
+++ src/main/java/Lf.java
@@ -3,5 +3,9 @@
 public class Lf {
     public static void main(String[] args) {
         System.out.println("TTTT");
+        System.out.println("TTTT");
+        System.out.println("TTTT");
+        System.out.println("TTTT");
+        System.out.println("TTTT");
     }
 }
